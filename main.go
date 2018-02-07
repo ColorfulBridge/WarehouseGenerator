616c0e9bb8d50ba54b6538f41643c4c273998f32
Michael Dietz <michael.dtz@gmail.com>
2018-02-07 20:07:48 +0100

    Simulate a warehouse at a given Polygon on a map (rectangle provided via geojson)

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"whs/wly"
+
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/encoding/geojson"
+)
+
+func main() {
+
+	data, err := ioutil.ReadFile("input/warehouse1.json")
+	check(err)
+
+	geometry := new(geom.T)
+	err = geojson.Unmarshal(data, geometry)
+	check(err)
+
+	polygon := (*geometry).(*geom.Polygon)
+
+	layout := wly.LayoutParameters{Horizontal : true, AisleSize : 10, RackSize : 8, Padding: 20 }
+	
+	wly.GenerateLayout(polygon, layout)
+
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
